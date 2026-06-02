e534bbfaad422be256cda3a0b3b7158102faa852
Sam Taggart <sam@sasworkshops.com>
2026-06-02 13:25:39 -0600

    Refactor: Whitespace cleanup

--- source/model.py
+++ source/model.py
@@ -2,11 +2,10 @@ class Product:
     def __init__(self, stock, hold):
         self.hold = hold
 
-
-
-    def addhold(self, quantity):
+    def add_hold(self, quantity):
         self.hold += quantity
+
 
 class Order:
     def add_item(self, product, quantity):
-        product.addhold(quantity)
+        product.add_hold(quantity)
