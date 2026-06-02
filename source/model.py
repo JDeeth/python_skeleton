0fb4fe7f5769cc1f67ef0520238ad4cef5fb7bc3
Sam Taggart <sam@sasworkshops.com>
2026-06-02 13:22:03 -0600

    Added second test for hold quantity.

--- source/model.py
+++ source/model.py
@@ -1,11 +1,13 @@
 class Product:
     def __init__(self, stock, hold):
-        pass
+        self._hold = hold
 
     def hold(self):
-        return 1
+        return self._hold
 
+    def addhold(self, quantity):
+        self._hold += quantity
 
 class Order:
     def add_item(self, product, quantity):
-        pass
+        product.addhold(quantity)
--- test/add_item_test.py
+++ test/add_item_test.py
@@ -9,7 +9,11 @@ class AddItem(unittest.TestCase):
         order = Order()
         order.add_item(product, 1)
         self.assertEqual(1, product.hold())
-
+    def test_AddItemWithQtyTwoThenHoldTwo(self):
+        product = Product(stock=7, hold=0)
+        order = Order()
+        order.add_item(product, 2)
+        self.assertEqual(2, product.hold())
 
 if __name__ == '__main__':
     unittest.main()
