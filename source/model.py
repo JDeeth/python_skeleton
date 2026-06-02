edde669de70c8f6b9c4a99a0420650f47f04bf8d
Sam Taggart <sam@sasworkshops.com>
2026-06-02 13:23:43 -0600

    Refactor: made hold function into field

--- source/model.py
+++ source/model.py
@@ -1,12 +1,11 @@
 class Product:
     def __init__(self, stock, hold):
-        self._hold = hold
+        self.hold = hold
 
-    def hold(self):
-        return self._hold
+
 
     def addhold(self, quantity):
-        self._hold += quantity
+        self.hold += quantity
 
 class Order:
     def add_item(self, product, quantity):
