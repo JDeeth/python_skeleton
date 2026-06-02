1393002643ff5b14ec53f061aa6cb910a7879900
Philip Mallegol-Hansen <philip.mh@slalom.com>
2026-06-02 12:33:42 -0700

    Refactor: Splits up Given/When/Then in the tests

--- test/add_item_test.py
+++ test/add_item_test.py
@@ -7,12 +7,17 @@ class AddItem(unittest.TestCase):
     def test_AddItemWithSufficientStock(self):
         product = Product(stock=7, hold=0)
         order = Order()
+
         order.add_item(product, 1)
+
         self.assertEqual(1, product.hold)
+
     def test_AddItemWithQtyTwoThenHoldTwo(self):
         product = Product(stock=7, hold=0)
         order = Order()
+
         order.add_item(product, 2)
+
         self.assertEqual(2, product.hold)
 
 if __name__ == '__main__':
